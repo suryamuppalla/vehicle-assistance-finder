6a69425403c9272b4a6df4b229b3637121428687
suryamuppalla <suryamuppalla@gmail.com>
2022-05-05 14:45:06 +0530

    requests table created

--- database/migrations/2022_05_04_010618_create_requests_table.php
+++ database/migrations/2022_05_04_010618_create_requests_table.php
@@ -15,19 +15,11 @@ return new class extends Migration
     {
         Schema::create('requests', function (Blueprint $table) {
             $table->increments('id', true);
-            // $table->unsignedInteger('user_id');
-            // $table->unsignedInteger('mechanic_id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('mechanic_id');
             $table->string('requested_address');
             $table->integer('requested_pincode');
             $table->timestamps();
-        });
-
-        Schema::table('requests', function(Blueprint $table) {
-
-            // $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreignId('garage_id')->constrained('garages')->onUpdate('cascade')->onDelete('cascade');
-            // $table->foreign('user_id')->references('id')->on('users');
-            // $table->foreign('mechanic_id')->references('id')->on('mechanics');
         });
     }
 
