11a4769d4f7a12f4c5d7fc5bfb3e1a7dc9f0eb73
suryamuppalla <suryamuppalla@gmail.com>
2022-05-05 17:20:55 +0530

    Register completed

--- resources/frontend/src/app/app.component.ts
+++ resources/frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  public isAuthentication = false;
+
+  constructor(
+    private router: Router
+  ) {
+    this.router.events.subscribe((ev) => {
+      if(ev instanceof NavigationEnd) {
+        this.isAuthentication = this.router.url.indexOf('/register') > -1 || this.router.url.indexOf('/login') > -1;
+      }
+    });
+  }
 }
