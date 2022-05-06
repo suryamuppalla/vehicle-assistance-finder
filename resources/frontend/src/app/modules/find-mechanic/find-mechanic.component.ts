4cdd399f94ef2a847abbdd842bd7eadff675c47f
suryamuppalla <suryamuppalla@gmail.com>
2022-05-06 18:50:18 +0530

    Find mechanics list

--- resources/frontend/src/app/modules/find-mechanic/find-mechanic.component.ts
+++ resources/frontend/src/app/modules/find-mechanic/find-mechanic.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
 import { ApplicationService } from 'src/app/services/application.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-find-mechanic',
@@ -9,13 +11,32 @@ import { ApplicationService } from 'src/app/services/application.service';
 })
 export class FindMechanicComponent implements OnInit {
 
+  public garages: any[] = [];
+  public imageUrl = environment.API.replace('/public/api', '');
   constructor(
     private router: Router,
-    public applicationService: ApplicationService
+    public applicationService: ApplicationService,
+    private httpClient: HttpClient
   ) {
   }
 
   ngOnInit(): void {
+    this.getGarages();
   }
 
+  getGarages() {
+    this.httpClient.get(
+      `${environment.API}/garages`
+    ).subscribe((response: any) => {
+      console.log(response);
+      this.garages = response;
+      if (this.garages?.length) {
+        this.garages?.forEach(item => {
+          item.experience_in = item?.experience_in?.split(',');
+        });
+      }
+    }, (error: any) => {
+      console.log(error);
+    });
+  }
 }
